64aa61689351fb51689b7cfc943abbd458fbf21a
Lucas Busso <lucasxbusso@gmail.com>
2025-01-10 09:59:29 -0300

    feat: recurso PUT para actualizar customer endpoint

--- src/main/java/microservicios_poc/controllers/CustomerRestController.java
+++ src/main/java/microservicios_poc/controllers/CustomerRestController.java
@@ -2,22 +2,22 @@ package microservicios_poc.controllers;
 
 import microservicios_poc.domain.Customer;
 import microservicios_poc.mocks.CustomersMockData;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 public class CustomerRestController {
+    private static final List<Customer> CUSTOMERS = CustomersMockData.getCustomers();
 
-    @GetMapping("customers")
+    @GetMapping("/customers")
     public List<Customer> getCustomers() {
-        return CustomersMockData.getCustomers();
+        return CUSTOMERS;
     }
 
-    @GetMapping("customers/{username}")
+    @GetMapping("/customers/{username}")
     public Customer getCustomer(@PathVariable String username) {
-       for(Customer item : CustomersMockData.getCustomers() ) {
+       for(Customer item : CUSTOMERS ) {
            if(item.getUserName().equals(username)) {
                return item;
            }
@@ -29,6 +29,19 @@ public class CustomerRestController {
     public Customer postCustomer(@RequestBody Customer customerAdded) {
       CUSTOMERS.add(customerAdded);
         return customerAdded;
+    }
+
+    @PutMapping("/customers/{id}")
+    public Customer updateCustomer(@RequestBody Customer customerUpdated, @PathVariable int id) {
+        for(Customer item : CUSTOMERS ) {
+            if(item.getId() == id) {
+                item.setUserName(customerUpdated.getUserName());
+                item.setName(customerUpdated.getName());
+                item.setPassword(customerUpdated.getPassword());
+                return item;
+            }
+        }
+        return null;
     }
 
 }
